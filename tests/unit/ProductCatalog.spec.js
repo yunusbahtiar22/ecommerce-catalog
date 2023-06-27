35854f2a382600df018c7905700ccb6afb6957bb
yunusbahtiar22 <aboomarmoshab22@gmail.com>
2023-06-27 21:09:08 +0700

    update test for `ProductCatalog.vue`

--- tests/unit/ProductCatalog.spec.js
+++ tests/unit/ProductCatalog.spec.js
@@ -1,18 +1,34 @@
-import { mount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 import ProductCatalog from "@/components/ProductCatalog.vue";
+import ProductCard from "@/components/ProductCard.vue";
+import ProductCardBase from "@/components/ProductCardBase.vue";
+import ProductCardError from "@/components/ProductCardError.vue";
 
 global.fetch = jest.fn();
 
 describe("ProductCatalog.vue", () => {
-  const wrapper = mount(ProductCatalog);
-  const product = wrapper.findComponent({ name: "ProductCard" });
+  const wrapper = shallowMount(ProductCatalog);
 
   beforeEach(() => {
     wrapper.vm.productIndex = 1;
+    wrapper.vm.loading = false;
+    wrapper.vm.error = false;
   });
 
-  it("renders ProductCard if is not loading", () => {
-    expect(product.exists()).toBe(true);
+  it("renders ProductCard if is not loading and not error", () => {
+    expect(wrapper.findComponent(ProductCard).exists()).toBe(true);
+  });
+
+  it("renders ProductCardBase when loading", async () => {
+    wrapper.vm.loading = true;
+    await wrapper.vm.$nextTick();
+    expect(wrapper.findComponent(ProductCardBase).exists()).toBe(true);
+  });
+
+  it("renders ProductCardError when error", async () => {
+    wrapper.vm.error = true;
+    await wrapper.vm.$nextTick();
+    expect(wrapper.findComponent(ProductCardError).exists()).toBe(true);
   });
 
   it("makes api call when mounted", () => {
